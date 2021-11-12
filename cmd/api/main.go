bb727a578898b30c9513d040f5f256a555fedf70
nickherrig <neherrig@gmail.com>
2021-11-11 20:18:04 -0600

    Add env var for DSN

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -34,7 +34,7 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "port for api")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:mysecretpassword@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
 	flag.Parse()
 
