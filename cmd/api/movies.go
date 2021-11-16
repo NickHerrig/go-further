5dd7b4ad91328c567b899b3734286f5be71b6f5c
nickherrig <neherrig@gmail.com>
2021-11-16 09:13:31 -0600

    Listing all movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -206,6 +206,15 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.storage.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 
 }
