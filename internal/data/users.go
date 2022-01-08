61119226111f4af76871a93aa137623dd5811666
nickherrig <neherrig@gmail.com>
2022-01-08 10:44:35 -0600

    user, password, email validation logic

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,13 +1,19 @@
 package data
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	"greenlight.nickherrig.com/internal/validator"
+)
 
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Passowrd  password  `json:"-"`
+	Password  password  `json:"-"`
 	Activated bool      `json:"activated:`
 	Version   int       `json:"-"`
 }
@@ -18,9 +24,55 @@ type password struct {
 }
 
 func (p *password) Set(plaintextPassword string) error {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
 	return nil
 }
 
 func (p *password) Matches(plaintextPassword string) (bool, error) {
-	return false, nil
+
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePassword(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes")
+	v.Check(len(password) <= 72, "password", "must be less than 72 bytes")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "user", "must be provided")
+	v.Check(len(user.Name) <= 500, "user", "must be less than 500 bytes")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePassword(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing hash for user password")
+	}
 }
