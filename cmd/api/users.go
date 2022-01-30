f9b5b360ed00e5b481729d2c832ade126cebcab0
nickherrig <neherrig@gmail.com>
2022-01-30 15:20:30 -0600

    Sending activation tokens via email

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.nickherrig.com/internal/data"
 	"greenlight.nickherrig.com/internal/validator"
@@ -51,8 +52,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.storage.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "welcome.tmpl", user)
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
