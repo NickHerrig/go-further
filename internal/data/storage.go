69caad943cf96e9e31889d2e090b038466087147
nickherrig <neherrig@gmail.com>
2022-01-30 14:35:49 -0600

    Add Token database table and methods for creating/storing activation tokens

--- internal/data/storage.go
+++ internal/data/storage.go
@@ -8,17 +8,19 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("Woops, we coudn't find that record")
-	ErrEditConflict   = errors.New("Woops, there are an edit conlfict")
+	ErrEditConflict   = errors.New("Woops, there was an edit conlfict")
 )
 
 type Storage struct {
 	Movies MovieStorage
 	Users  UserStorage
+	Tokens TokenStorage
 }
 
 func NewStorage(db *pgxpool.Pool) Storage {
 	return Storage{
 		Movies: MovieStorage{DB: db},
 		Users:  UserStorage{DB: db},
+		Tokens: TokenStorage{DB: db},
 	}
 }
